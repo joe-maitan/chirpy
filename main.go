4d9550a835d7bffdb10cacb79b1a66f3e8a27246
joe-maitan <jjmaitan72@gmail.com>
2026-01-12 17:12:16 -0700

    Working on Chirpy

--- main.go
+++ main.go
@@ -12,13 +12,19 @@ func main() {
 	mux := http.NewServeMux()
 	port := "8080" // os.Getenv("PORT")
 	
+	/* A http.Server is a struct that describes a server configuration */
 	server := http.Server{
 		Handler: mux,
-		Addr: 	 ":" + port,
+		Addr: 	 "127.0.0.1:" + port,
 
 	} 
+
+	mux.Handle("/", http.FileServer(http.Dir(".")))
 	
 	fmt.Printf("Server started on: %v...\n", port)
+	
+	/* ListenAndServe() blocks the main function until the server shuts down or an
+	unexpected error crashes it. */
 	err := server.ListenAndServe()
 	log.Fatal(err)
 } // End main() func
