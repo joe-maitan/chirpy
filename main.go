dd1faede2fbafc56bce5cc448c2e08dcf480032e
joe-maitan <jjmaitan72@gmail.com>
2026-01-16 12:15:17 -0700

    Finished Chapter 2

--- main.go
+++ main.go
@@ -5,29 +5,59 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+} // End apiConfig struct
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
-}
+} // End handlerReadiness() func
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+} // End handlerMetrics() func
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
+} // End handlerReset() func
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+} // End middlewareMetricsInc() func
 
 func main() {
-	port := "8080" // os.Getenv("PORT")
+	const filepathRoot = "."
+	const port = "8080" // os.Getenv("PORT")
+
+	
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	// mux.Handle("/assets", http.FileServer(http.Dir("./assets/logo.png")))
-	
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", handlerReadiness)
+	// mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	// Method specific routing. [METHOD ][HOST]/[PATH]
+	mux.HandleFunc("GET /healthz", handlerReadiness)
+	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /reset", apiCfg.handlerReset)
 
 	/* A http.Server is a struct that describes a server configuration */
 	server := http.Server{
 		Handler: mux,
 		Addr: 	 "127.0.0.1:" + port,
-
 	} 
 
 	fmt.Printf("Server started on: %v...\n", port)
