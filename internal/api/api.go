1b6a8fcd7c495a82d576c7fb1532f625eafe6e4e
joe-maitan <jjmaitan72@gmail.com>
2026-03-01 15:23:45 -0700

    Add a way for a user to update email and pw

--- internal/api/api.go
+++ internal/api/api.go
@@ -217,7 +217,65 @@ func (cfg *Config) HandleUserLogin(w http.ResponseWriter, r *http.Request) {
 } // End HandleUserLogin() func
 
 func (cfg *Config) HandleUpdateUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
 
+	type Response struct {
+		ID	uuid.UUID `json:"id"`
+		Email string `json:"email"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		RespondWithError(w, http.StatusUnauthorized, "Missing authentication token", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.JWTSecret)
+	if err != nil {
+		log.Printf("api.go - HandleUpdateUser() - Error trying to call auth.ValidateJWT: %v", err)
+		RespondWithError(w, http.StatusUnauthorized, "Could not validate JWT", err)
+		return
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err = decoder.Decode(&params)
+	if err != nil {
+		log.Printf("api.go - HandleUpdateUser() - Error decoding parameters: %v", err)
+		RespondWithError(w, http.StatusUnauthorized, "Error decoding parameters", err)
+		return
+	}
+
+	hashedPassword, err := auth.HashPassword(params.Password)
+	if err != nil {
+		log.Printf("api.go - HandleUpdateUser() - Error hashing password: %v", err)
+		RespondWithError(w, http.StatusInternalServerError, "Error hashing password", err)
+		return
+	}
+
+	user, err := cfg.DB.UpdateUserPassword(r.Context(), database.UpdateUserPasswordParams{
+		ID:             userID,
+		Email:          params.Email,
+		HashedPassword: hashedPassword,
+	})
+
+	if err != nil {
+		log.Printf("api.go - HandleUpdateUser() - Error updating user: %v", err)
+		RespondWithError(w, http.StatusInternalServerError, "Error updating user", err)
+		return
+	}
+
+	RespondWithJSON(w, http.StatusOK, Response{
+		ID: user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Email: user.Email,
+	})
 } // End HandleUpdateUser() func
 
 func (cfg *Config) HandleCreateChirp(w http.ResponseWriter, r *http.Request) {
@@ -228,7 +286,7 @@ func (cfg *Config) HandleCreateChirp(w http.ResponseWriter, r *http.Request) {
 	}
 
 	type parameters struct {
-		Body   string    `json:"body"`
+		Body string `json:"body"`
 		// UserID uuid.UUID `json:"user_id"`
 	}
 
--- internal/database/001_users.sql.go
+++ internal/database/001_users.sql.go
@@ -7,6 +7,9 @@ package database
 
 import (
 	"context"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 const createUser = `-- name: CreateUser :one
@@ -71,6 +74,38 @@ func (q *Queries) GetUserByEmail(ctx context.Context, email string) (User, error
 		&i.UpdatedAt,
 		&i.Email,
 		&i.HashedPassword,
+	)
+	return i, err
+}
+
+const updateUserPassword = `-- name: UpdateUserPassword :one
+UPDATE users
+SET email = $2, hashed_password = $3, updated_at = NOW()
+WHERE id = $1
+RETURNING id, created_at, updated_at, email
+`
+
+type UpdateUserPasswordParams struct {
+	ID             uuid.UUID
+	Email          string
+	HashedPassword string
+}
+
+type UpdateUserPasswordRow struct {
+	ID        uuid.UUID
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Email     string
+}
+
+func (q *Queries) UpdateUserPassword(ctx context.Context, arg UpdateUserPasswordParams) (UpdateUserPasswordRow, error) {
+	row := q.db.QueryRowContext(ctx, updateUserPassword, arg.ID, arg.Email, arg.HashedPassword)
+	var i UpdateUserPasswordRow
+	err := row.Scan(
+		&i.ID,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+		&i.Email,
 	)
 	return i, err
 }
