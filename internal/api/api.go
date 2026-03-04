ff59e5ff0e77dab87abeded898f298ae609f863e
joe-maitan <jjmaitan72@gmail.com>
2026-03-04 08:38:49 -0700

    Create json.go

--- internal/api/api.go
+++ internal/api/api.go
@@ -40,36 +40,7 @@ type Chirp struct {
 	UserID    uuid.UUID `json:"user_id"`
 } // End Chirp struct
 
-func RespondWithError(w http.ResponseWriter, code int, msg string, err error) {
-	if err != nil {
-		log.Println(err)
-	}
 
-	if code > 499 {
-		log.Printf("Responding with 5XX error: %s", msg)
-	}
-
-	type errorResponse struct {
-		Error string `json:"error"`
-	}
-
-	RespondWithJSON(w, code, errorResponse{
-		Error: msg,
-	})
-} // End RespondWithError() func
-
-func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-
-	data, err := json.Marshal(payload)
-	if err != nil {
-		log.Printf("Error marshalling JSON: %s", err)
-		w.WriteHeader(500)
-	}
-
-	w.WriteHeader(code)
-	w.Write(data)
-} // End RespondWithJSON() func
 
 func (cfg *Config) HandlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
