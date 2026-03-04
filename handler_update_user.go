4c6f61c708b7db972f3939b5c7f8996acd358b04
joe-maitan <jjmaitan72@gmail.com>
2026-03-04 09:03:20 -0700

    Add handle_update_user.go

---
+++ handler_update_user.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"log"
+	"time"
+	"net/http"
+	"encoding/json"
+
+	"github.com/google/uuid"
+	"github.com/joe-maitan/chirpy/internal/auth"
+	"github.com/joe-maitan/chirpy/internal/database"
+)
+
+func (cfg *apiConfig) HandleUpdateUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	type Response struct {
+		ID	uuid.UUID `json:"id"`
+		Email string `json:"email"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		IsChirpyRed bool `json:"is_chirpy_red"`
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Missing authentication token", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		log.Printf("api.go - HandleUpdateUser() - Error trying to call auth.ValidateJWT: %v", err)
+		respondWithError(w, http.StatusUnauthorized, "Could not validate JWT", err)
+		return
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err = decoder.Decode(&params)
+	if err != nil {
+		log.Printf("api.go - HandleUpdateUser() - Error decoding parameters: %v", err)
+		respondWithError(w, http.StatusUnauthorized, "Error decoding parameters", err)
+		return
+	}
+
+	hashedPassword, err := auth.HashPassword(params.Password)
+	if err != nil {
+		log.Printf("api.go - HandleUpdateUser() - Error hashing password: %v", err)
+		respondWithError(w, http.StatusInternalServerError, "Error hashing password", err)
+		return
+	}
+
+	user, err := cfg.db.UpdateUserPassword(r.Context(), database.UpdateUserPasswordParams{
+		ID:             userID,
+		Email:          params.Email,
+		HashedPassword: hashedPassword,
+	})
+
+	if err != nil {
+		log.Printf("api.go - HandleUpdateUser() - Error updating user: %v", err)
+		respondWithError(w, http.StatusInternalServerError, "Error updating user", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, Response{
+		ID: user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Email: user.Email,
+		IsChirpyRed: user.IsChirpyRed,
+	})
+} // End HandleUpdateUser() func
--- internal/api/api.go
+++ internal/api/api.go
@@ -47,69 +47,7 @@ type Chirp struct {
 
 
 
-func (cfg *Config) HandleUpdateUser(w http.ResponseWriter, r *http.Request) {
-	type parameters struct {
-		Email    string `json:"email"`
-		Password string `json:"password"`
-	}
 
-	type Response struct {
-		ID	uuid.UUID `json:"id"`
-		Email string `json:"email"`
-		CreatedAt time.Time `json:"created_at"`
-		UpdatedAt time.Time `json:"updated_at"`
-		IsChirpyRed bool `json:"is_chirpy_red"`
-	}
-
-	token, err := auth.GetBearerToken(r.Header)
-	if err != nil {
-		RespondWithError(w, http.StatusUnauthorized, "Missing authentication token", err)
-		return
-	}
-
-	userID, err := auth.ValidateJWT(token, cfg.JWTSecret)
-	if err != nil {
-		log.Printf("api.go - HandleUpdateUser() - Error trying to call auth.ValidateJWT: %v", err)
-		RespondWithError(w, http.StatusUnauthorized, "Could not validate JWT", err)
-		return
-	}
-
-	decoder := json.NewDecoder(r.Body)
-	params := parameters{}
-	err = decoder.Decode(&params)
-	if err != nil {
-		log.Printf("api.go - HandleUpdateUser() - Error decoding parameters: %v", err)
-		RespondWithError(w, http.StatusUnauthorized, "Error decoding parameters", err)
-		return
-	}
-
-	hashedPassword, err := auth.HashPassword(params.Password)
-	if err != nil {
-		log.Printf("api.go - HandleUpdateUser() - Error hashing password: %v", err)
-		RespondWithError(w, http.StatusInternalServerError, "Error hashing password", err)
-		return
-	}
-
-	user, err := cfg.DB.UpdateUserPassword(r.Context(), database.UpdateUserPasswordParams{
-		ID:             userID,
-		Email:          params.Email,
-		HashedPassword: hashedPassword,
-	})
-
-	if err != nil {
-		log.Printf("api.go - HandleUpdateUser() - Error updating user: %v", err)
-		RespondWithError(w, http.StatusInternalServerError, "Error updating user", err)
-		return
-	}
-
-	RespondWithJSON(w, http.StatusOK, Response{
-		ID: user.ID,
-		CreatedAt: user.CreatedAt,
-		UpdatedAt: user.UpdatedAt,
-		Email: user.Email,
-		IsChirpyRed: user.IsChirpyRed,
-	})
-} // End HandleUpdateUser() func
 
 func (cfg *Config) HandleCreateChirp(w http.ResponseWriter, r *http.Request) {
 	badWords := []string{
