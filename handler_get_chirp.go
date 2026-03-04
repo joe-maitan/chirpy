2fb2ca0f3d0755bd158f8e4ee2c2a9155beeacbd
joe-maitan <jjmaitan72@gmail.com>
2026-03-04 13:49:07 -0700

    Finish handler_get_chirp.go

--- handler_get_chirp.go
+++ handler_get_chirp.go
@@ -1,17 +1,40 @@
 package main
 
 import (
+	"sort"
 	"net/http"
 
 	"github.com/google/uuid"
+	"github.com/joe-maitan/chirpy/internal/database"
 )
 
 func (cfg *apiConfig) HandleGetAllChirps(w http.ResponseWriter, r *http.Request) {
-	DBChirps, err := cfg.db.GetAllChirps(r.Context())
-	if err != nil {
-		logStatement("handler_get_chirp", "HandleGetAllChirps()", "Error getting all chirps", err)
-		respondWithError(w, http.StatusInternalServerError, "Error getting all chirps", err)
-		return
+	var DBChirps []database.Chirp
+	var err error
+
+	authorID := r.URL.Query().Get("author_id")
+	sortOrder := r.URL.Query().Get("sort")
+	if authorID != "" {
+		// logStatement("handler_get_chirp.go", "HandleGetAllChirps()", fmt.Sprintf("authorID=%v", authorID), nil)
+		data, err := uuid.Parse(authorID)
+		if err != nil {
+			respondWithError(w, http.StatusNotFound, "Error parsing authorID as a uuid", err)
+			return
+		}
+
+		DBChirps, err = cfg.db.GetAllChripsByUser(r.Context(), data)
+		if err != nil {
+			// logStatement("handler_get_chirp", "HandleGetAllChirps()", "Error getting all chirps", err)
+			respondWithError(w, http.StatusInternalServerError, "Error getting all chirps", err)
+			return
+		}
+	} else {
+		DBChirps, err = cfg.db.GetAllChirps(r.Context())
+		if err != nil {
+			logStatement("handler_get_chirp", "HandleGetAllChirps()", "Error getting all chirps", err)
+			respondWithError(w, http.StatusInternalServerError, "Error getting all chirps", err)
+			return
+		}
 	}
 
 	chirps := []Chirp{}
@@ -22,6 +45,16 @@ func (cfg *apiConfig) HandleGetAllChirps(w http.ResponseWriter, r *http.Request)
 			UpdatedAt: c.UpdatedAt,
 			Body:      c.Body,
 			UserID:    c.UserID,
+		})
+	}
+
+	if sortOrder == "asc" {
+		sort.Slice(chirps, func(i, j int) bool {
+			return chirps[i].CreatedAt.Before(chirps[j].CreatedAt)
+		})
+	} else if sortOrder == "desc" {
+		sort.Slice(chirps, func(i, j int) bool {
+			return chirps[i].CreatedAt.After(chirps[j].CreatedAt)
 		})
 	}
 
