b0c195d25fc473301e7546f8fead7d35b588ada1
joe-maitan <jjmaitan72@gmail.com>
2026-03-04 13:10:51 -0700

    Group admin functions handler_admin.go file

--- handler_admin.go
+++ handler_admin.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -15,3 +16,10 @@ func (cfg *apiConfig) HandlerReset(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hits reset to 0"))
 } // End HandlerReset() func
+
+func (cfg *apiConfig) HandlerMetrics(w http.ResponseWriter, r *http.Request) {
+	// Display metrics to the admin user
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte(fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", cfg.fileServerHits.Load())))
+} // End handlerMetrics() func
--- handler_middleware.go
+++ handler_middleware.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
@@ -11,13 +10,6 @@ func (cfg *apiConfig) HandlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 	w.Write([]byte(http.StatusText(200)))
 } // End HandlerReadiness() func
-
-func (cfg *apiConfig) HandlerMetrics(w http.ResponseWriter, r *http.Request) {
-	// Display metrics to the admin user
-	w.Header().Add("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(200)
-	w.Write([]byte(fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", cfg.fileServerHits.Load())))
-} // End handlerMetrics() func
 
 func (cfg *apiConfig) MiddlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
